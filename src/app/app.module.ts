c9ac4c7b865f68a749e6e178d6d2c345111f08c3
Diego Rojas <diego-fernando.rojas@getronics.com>
2022-04-26 12:16:39 +0200

    Added component Servers & use of String Interpolation, Property Binding, Event Handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { SuccessAlertComponent } from './successAlert/successAlert.component';
 import { WarningAlertComponent } from './warningAlert/warningAlert.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     SuccessAlertComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
